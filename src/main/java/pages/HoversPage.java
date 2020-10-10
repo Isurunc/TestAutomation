ab2fa25cbb6fa80ed7903333f06baa4043fd8870
Isuru Piyarathna <IsuruP@compello.lk>
2020-10-10 20:12:59 +0530

    Added HoverOverTest

--- src/main/java/pages/HoversPage.java
+++ src/main/java/pages/HoversPage.java
@@ -1,19 +1,55 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class HoversPage {
 
     private WebDriver driver;
-    private By hoverOver = By.
+    private By figureBox = By.className("figure");
+    private By boxCaption = By.className("figcaption");
 
-    public  HoversPage (WebDriver driver) {
+    public HoversPage (WebDriver driver) {
         this.driver = driver;
     }
-   public Void hoverOverFigures (int index) {
+
+    /**
+     *
+     * @param index starts at 1
+     *
+     */
+   public FigureCaption hoverOverFigures (int index) {
+       WebElement figure = driver.findElements(figureBox).get(index - 1);
        Actions actions = new Actions(driver);
-       actions.moveToElement()
+       actions.moveToElement(figure).perform();
+
+       return new FigureCaption(figure.findElement(boxCaption));
    }
 
+   //this is a nested class
+   public class FigureCaption {
+
+       private WebElement caption;
+       private By header = By.tagName("h5");
+       private By link = By.tagName("a");
+
+    public FigureCaption (WebElement caption) {
+        this.caption = caption;
+    }
+    public boolean isCaptionDisplayed () {
+        return caption.isDisplayed();
+    }
+    public String getTitle () {
+        return caption.findElement(header).getText();
+    }
+    public String getLink () {
+        return caption.findElement(link).getAttribute("href");
+    }
+    public String getLinkText () {
+        return caption.findElement(link).getText();
+    }
+
+   }
 }
